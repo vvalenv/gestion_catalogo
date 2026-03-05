f257ab219c917897ae14f64896015cae79f89725
val <valenvillar.neco@gmail.com>
2026-03-05 15:04:44 -0300

    data config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,14 @@
 const db = { 
-    host: 'localhost',
-    user: 'val',
-    database: 'gonzalez',
-    password: 'valAdmin'
+    host: 'mysql-xxxxx-tu-host.aivencloud.com', // El de Aiven
+    port: 14724,                                 // El puerto que te dio Aiven
+    user: 'avnadmin',
+    password: 'tu_password_de_aiven',
+    database: 'defaultdb',                       // O 'gonzalez' si la creaste
+    ssl: {
+        rejectUnauthorized: false                // Obligatorio para Aiven
+    },
+    waitForConnections: true,
+    connectionLimit: 10
 };
 
 export default db;
