82257780df658c1807f2ced190d2d20d37cc93f3
val <valenvillar.neco@gmail.com>
2026-03-05 15:10:48 -0300

    correccion de nombres

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 const db = { 
-    host: 'mysql-xxxxx-tu-host.aivencloud.com', // El de Aiven
+    host: 'mysql-2dbac228-gonzalez.i.aivencloud.com', // El de Aiven
     port: 14724,                                 // El puerto que te dio Aiven
     user: 'avnadmin',
-    password: 'tu_password_de_aiven',
+    password: 'AVNS_mmFJUAw6ttFfLJAGLvR',
     database: 'defaultdb',                       // O 'gonzalez' si la creaste
     ssl: {
         rejectUnauthorized: false                // Obligatorio para Aiven
